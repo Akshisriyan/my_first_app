d26f6f0d80584c769b81f9cc7648269bb30b599a
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2023-06-20 10:03:22 +0530

    update

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const operatorColor = Color.fromARGB(255, 0, 0, 0);
+const buttonColor = Color(0xFF212121);
+const orangeColor = Color.fromARGB(255, 218, 103, 21);
