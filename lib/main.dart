4620e84ec8e09e322305afbb5072e1ea83ebb418
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2023-06-22 23:04:57 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/colors.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -22,18 +23,37 @@ class CalculatorAPPState extends State<CalculatorAPP> {
         children: [
           Expanded(
               child: Container(
-            color: Colors.red,
+            color: const Color.fromARGB(255, 247, 246, 246),
           )),
           //buttons area
 
           Row(
-            children: [
-              Expanded(
-                  child: ElevatedButton(onPressed: () {}, child: Text("1"))),
-            ],
+            children: [button(text: "AC"), button(text: "<")],
           )
         ],
       ),
     );
   }
+}
+
+Widget button({text, tcolor = Colors.white, buttonBgColor = buttonColor}) {
+  return Expanded(
+      child: Container(
+          margin: EdgeInsets.all(8),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+                padding: const EdgeInsets.all(22),
+                primary: buttonBgColor,
+              ),
+              onPressed: () {},
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: tcolor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ))));
 }
